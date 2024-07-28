2967207dc1ad0d176c253f7797a592646762bd99
Tetiana Kotolup <93287415+tetiana-a@users.noreply.github.com>
2024-07-28 11:34:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,41 @@ function toggleMenu() {
   icon.classList.toggle("open");
 }
 
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const audio = document.getElementById('audio');
+  const playPauseButton = document.getElementById('play-pause');
+  const seekBar = document.getElementById('seek-bar');
+  const currentTimeSpan = document.getElementById('current-time');
+
+  playPauseButton.addEventListener('click', function () {
+      if (audio.paused) {
+          audio.play();
+          playPauseButton.classList.remove('play');
+          playPauseButton.classList.add('pause');
+      } else {
+          audio.pause();
+          playPauseButton.classList.remove('pause');
+          playPauseButton.classList.add('play');
+      }
+  });
+
+  audio.addEventListener('timeupdate', function () {
+      const currentTime = formatTime(audio.currentTime);
+      currentTimeSpan.textContent = currentTime;
+      seekBar.value = (audio.currentTime / audio.duration) * 100;
+  });
+
+  seekBar.addEventListener('input', function () {
+      audio.currentTime = (seekBar.value / 100) * audio.duration;
+  });
+
+  function formatTime(seconds) {
+      const minutes = Math.floor(seconds / 60);
+      seconds = Math.floor(seconds % 60);
+      return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+  }
+});
+
+
